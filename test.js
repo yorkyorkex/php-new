4f9f8e09e81878b786fa1b037de64bbb62c322d4
York Yu <n11453559@gmail.com>
2025-11-02 16:50:39 +1100

    Add basic HTTP server (listen on :3000), reorder imports, and comment out legacy fs read/write flow

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 const fs = require('fs')
+const http = require('http')
+const url = require('url')
+//file
 
-fs.readFile('./input.txt', 'utf-8', (err, data1) => {
+//server
+const server = http
+  .createServer((req, res) => {
+    console.log('Request URL:', req.url)
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+    res.end('Hello World\n')
+  })
+  .listen(3000, () => {
+    console.log('Server running at http://localhost:3000/')
+  })
+
+/* fs.readFile('./input.txt', 'utf-8', (err, data1) => {
+  if (err) {
+    console.error('Error reading input.txt:', err)
+    return
+  }
   fs.readFile(`./${data1}.txt`, 'utf-8', (err, data2) => {
     fs.readFile('./append.txt', 'utf-8', (err, data3) => {
       fs.writeFile('./output.txt', `${data1} ${data2} ${data3}`, (err) => {
@@ -10,3 +29,4 @@ fs.readFile('./input.txt', 'utf-8', (err, data1) => {
     })
   })
 })
+ */
